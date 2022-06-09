0a94dc7011b0860af044228c729287a3ac0ff124
Stas Buchnev <Figor.figo@yandex.ru>
2022-06-09 11:05:51 +0300

    add solution java Syntax from task 1515

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1515/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task15.task1515;
+
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDateTime;
+import java.util.Scanner;
+
+/* 
+Абсолютный путь
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        Path path = Path.of(str);
+
+
+        if (path.isAbsolute()) {
+            System.out.println(path);
+        } else {
+
+            System.out.println(path.toAbsolutePath());
+        }
+    }
+}
+
+
+
